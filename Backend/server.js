0399e28808b15be80548a2030b8201434fe9cd26
Balasai <balasaiterli754@gmail.com>
2024-11-20 20:40:48 +0530

    added images

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors")
 const dbConnection = require("./src/dbConnection");
 const signupController = require("./src/controllers/signup");
 const loginController = require("./src/controllers/login");
+const imageController = require("./src/controllers/Image"); // Add image controller
 
 const app = express();
 
 dotenv.config();
 app.use(express.json());
+app.use(cors())
 
 const PORT = process.env.PORT || 8000;
 
 app.post("/signup", signupController);
 app.post("/login",loginController)
+app.get("/images", imageController.getImages);
+
 
 dbConnection(process.env.DB_URI)
   .then(() => {
