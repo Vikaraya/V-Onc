664d853287a46621eca985640356e580281c4343
Sheshi8317665599 <vikrayaofficial@gmail.com>
2024-11-23 20:10:46 +0530

    first commit by ysp

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const cors = require('cors');
+
 const dotenv = require("dotenv");
 const dbConnection = require("./src/dbConnection");
 const signupController = require("./src/controllers/signup");
 const loginController = require("./src/controllers/login");
 
 const app = express();
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Parse JSON bodies
 
 dotenv.config();
 app.use(express.json());
@@ -17,6 +21,6 @@ app.post("/login",loginController)
 dbConnection(process.env.DB_URI)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is listening on port ${PORT}`);
+      console.log(`Server is listening on port ${8000}`);
     });
   })
